0a04cfcf5a16ec47543f8af6dcb54e6eaa9a53f9
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2022-01-12 17:46:27 +0100

    remove promise

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,16 @@ export default (() => {
   let app: connect.Server = connect()
 
   const startServer = (port = 3000, dirname = '.') => {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        app
-          .use(serveStatic(dirname))
-          .listen(port, () => console.log(`Server running on ${port}`))
-        resolve()
-      } catch (error) {
-        reject(error)
-      }
-    })
+    // return new Promise<void>((resolve, reject) => {
+    try {
+      app
+        .use(serveStatic(dirname))
+        .listen(port, () => console.log(`Server running on ${port}`))
+      // resolve()
+    } catch (error) {
+      // reject(error)
+    }
+    // })
   }
 
   const stopServer = () => {

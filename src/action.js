ee2baca609eae14b475cfeb3e30c45a37dc55c6b
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2022-01-12 13:47:17 +0100

    test env variables

--- src/action.js
+++ src/action.js
@@ -1,8 +1,27 @@
-const io = require("@actions/io");
+const core = require("@actions/core");
+// const github = require("@actions/github");
+// const fs = require("fs");
+const path = require("path");
+// const readmeBox = require("readme-box").ReadmeBox;
+// const chunk = require("chunk");
 
-async function run() {
-  console.log("Hello world!");
-  await io.mkdirP("/helloworld");
-}
+(async () => {
+  // const githubToken = core.getInput("github-token");
+  // const filePath = path.join(
+  //   process.env.GITHUB_WORKSPACE,
+  //   core.getInput("json-file-path")
+  // );
 
-run();
+  // const columns = core.getInput("columns");
+  // const data = fs.readFileSync(filePath, "utf8");
+  // const json = JSON.parse(data);
+  // const fileToUsePath = core.getInput("file-to-use");
+
+  async function run() {
+    console.log("Hello world!");
+    console.log(process.env);
+    console.log(process.env.GITHUB_WORKSPACE);
+  }
+
+  run();
+})();

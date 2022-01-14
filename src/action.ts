bfd99c1bc1460488486c922b562ed1ec6e1f4f89
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2022-01-14 12:17:17 +0100

    trigger new run

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-// const github = require("@actions/github");
 // import { series } from 'async'
 // import { exec } from 'child_process'
 import puppeteer from './puppeteer'

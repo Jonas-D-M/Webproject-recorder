247dbd11584905304850051816113859ab91cc6c
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2022-01-19 14:35:46 +0100

    change base url

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -106,7 +106,7 @@ export default (() => {
     isStatic = false,
   ) => {
     const browser = await initBrowser(executablePath)
-    const url = 'http://localhost:3000'
+    const url = 'http://127.0.0.1:3000'
 
     try {
       const urlMap = generateUrlMap(sitemap, url, isStatic)
